1294082b4f01b4ee3098a7fdeebdbdb9c0657f63
Rohith <rohithmadesh919@gmail.com>
2026-03-26 23:02:06 +0530

    updated portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 function About() {
   return (
     <div>
-      <h1>Hi, I'm Rohith 👋</h1>
+      <h1>Hi, I'm Rohith<span className="emoji">👋</span></h1>
       <p>I am learning Full Stack Development</p>
     </div>
   );
--- src/components/Project.js
+++ src/components/Project.js
@@ -5,20 +5,31 @@ function Projects() {
 
   return (
     <div>
+
       <h2>🎮 Counter Game</h2>
 
       <h3>Score: {count}</h3>
-    
-    <div className='buttons'>
-      <button onClick={() => setCount(count + 1)}>
-        Increase 🔼
-      </button>
 
-      <button onClick={() => setCount(count - 1)}>
-        Decrease 🔽
-      </button>
-        <button onClick={() => setCount(0)}>🔁 Reset</button>
-        </div>
+      <div className='buttons'>
+        <button onClick={() => setCount(count + 1)}>
+          Increase ⬆️
+        </button>
+
+        <button onClick={() => setCount(count - 1)}>
+          Decrease ⬇️
+        </button>
+
+        <button onClick={() => setCount(0)}>
+          Reset 🔄
+        </button>
+      </div>
+
+      {/* ✅ ADD THIS PART */}
+      <div className="links">
+        <a href="https://rohith45-portfolio.netlify.app/" target="_blank">🔗 Live Demo</a>
+        <a href="https://github.com/rohithmadesh919-max/my-portfolio" target="_blank">💻 GitHub</a>
+      </div>
+
     </div>
   );
 }
